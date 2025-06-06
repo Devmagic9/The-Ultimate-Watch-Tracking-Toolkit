90a66b048e81929c765ba8ab496d837ad61ef0a5
Devmagic9 <serge.aka1@gmail.com>
2025-06-06 12:51:47 -0500

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,20 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import { VitePluginFonts } from 'vite-plugin-fonts';
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePluginFonts({
+      google: {
+        families: [
+          {
+            name: 'Inter',
+            styles: 'wght@400;500;600;700',
+          },
+        ],
+      },
+    }),
+  ],
+  base: '/The-Ultimate-Watch-Tracking-Toolkit/', // make sure this matches your GitHub repo name
 });
